a5a97e5a054a9828f51394473e250621f27f6906
ssimm333 <stanislavsimanovich1@gmail.com>
2024-11-14 13:11:04 +0000

    up to and including gotoTail added function declarations Doxygen not yet done Debugging may be needed

--- doubeLinkedList.c
+++ doubeLinkedList.c
@@ -14,19 +14,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "functionDeclarations.h"
+
+typedef int data;
 
 // defining the structure of a node in the double linked list
 typedef struct Node {
-	void* data;
+	data d;
 	struct Node* successor;			// pointer to the next node
 	struct Node* predecessor;		// pointer to the previous node
-} Node;
+} dllNode;
 
 
 // defining the structure for the double linked list
 typedef struct DoubleLinkedList {
-	Node* head;
-	Node* tail;
+	dllNode* head;
+	dllNode* tail;
+	dllNode* current;
 } DoubleLinkedList;
 
 
@@ -41,8 +45,8 @@ DoubleLinkedList* createDoubleLinkedList() {
 	DoubleLinkedList* doubleLinkedList = (DoubleLinkedList*) malloc(sizeof(DoubleLinkedList));
 
 	// allocating memory for the head and tail node
-	Node* head = (Node*) malloc(sizeof(Node));
-	Node* tail = (Node*) malloc(sizeof(Node));
+	dllNode* head = (dllNode*) malloc(sizeof(dllNode));
+	dllNode* tail = (dllNode*) malloc(sizeof(dllNode));
 
 	// initialising head and tail nodes
 	head -> predecessor = NULL;
@@ -59,7 +63,7 @@ DoubleLinkedList* createDoubleLinkedList() {
 }
 	
 // delete a list, including all its elements
-DoubleLinkedList* deleteDoubleLinkedList() {
+DoubleLinkedList* deleteDoubleLinkedList(DoubleLinkedList* list) {
 
 	// Pre: valid double linked list exists
 	if (list == NULL || list->head == NULL) {
@@ -69,9 +73,9 @@ DoubleLinkedList* deleteDoubleLinkedList() {
 	}
 
 	// go through the list and delete each node
-	Node* current = list->head;
+	dllNode* current = list->head;
 	while (current != NULL) {
-		Node* next = current->successor;
+		dllNode* next = current->successor;
 		free(current);
 		current = next;
 	}
@@ -81,5 +85,77 @@ DoubleLinkedList* deleteDoubleLinkedList() {
 
 	// Post: The entire list is deleted
 	return NULL;
+}
+
+// returns the data associated with current node
+DoubleLinkedList* getData(DoubleLinkedList* list) {
+	
+	// Pre: valid double linked list exists
+	if (list == NULL || list->head == NULL) {
+		printf("Error: Invalid double linked list\n");
+		return NULL;
+	}
+
+	// Post: data of current node is returned
+	return list->current->d;
+}
+
+// sets current node to the successor of current
+DoubleLinkedList* gotoNextNode(DoubleLinkedList* list) {
+
+	// Pre: valid double linked list exists
+	if (list == NULL || list->head == NULL) {
+		printf("Error: INvalud double linked list\n");
+		return NULL;
+	}
+
+	// Post: if current node is other than tail, current node is set to successor 
+	// of current node. Otherwise, list remains unchanged.
+	if (list->current != list->tail) {
+		list->current = list->current->successor;
+	}
+	return list->current;
+}
+
+// sets current node to predeccessor of current
+DoubleLinkedList* gotoPreviousNode(DoubleLinkedList* list) {
+	// Pre: valid double linked list exists
+	if (list == NULL || list->head == NULL) {
+		printf("Error: Invalid doube linked list\n");
+		return NULL;
+	}
+
+	// Post: if current node is other than head, current node is set to predeccessor 
+	// of current node. Otherwise, list remains unchanged.
+	if (list->current != list->head) {
+		list->current = list->current->predecessor;
+	}
+	return list->current;
+}
+
+// sets current node to head
+DoubleLinkedList* gotoHead(DoubleLinkedList* list) {
+	// Pre: valid double linked list exists
+	if (list == NULL || list->head == NULL) {
+		printf("Error: Invalid double linked list\n");
+		return NULL;
+	}
+
+	// Post: current node is set to head
+	list->current = list->head;
+	return list->current;
+}
+
+// sets current node to tail
+DoubleLinkedList* gotoTail(DoubleLinkedList* list) {
+	// Pre: valid double linked list exists
+	if (list == NULL || list->head == NULL) {
+		printf("Error: Invalid double linked list\n");
+		return NULL;
+	}
+
+	// Post: current node is set to tail
+	list->current = list->tail;
+	return list->current;
 }
 
---
+++ functionDeclarations.h
@@ -0,0 +1,11 @@
+#pragma once
+DoubleLinkedList* createDoubleLinkedList();
+DoubleLinkedList* deleteDoubleLinkedList(DoubleLinkedList* list);
+DoubleLinkedList* getData(DoubleLinkedList* list);
+DoubleLinkedList* gotoNextNode(DoubleLinkedList* list);
+DoubleLinkedList* gotoPreviousNode(DoubleLinkedList* list);
+DoubleLinkedList* gotoHead(DoubleLinkedList* list);
+DoubleLinkedList* gotoTail(DoubleLinkedList* list);
+DoubleLinkedList* insertAfter(newdata);
+DoubleLinkedList* insertBefore(newdata);
+DoubleLinkedList* deleteCurrent();
